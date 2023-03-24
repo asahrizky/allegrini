d73e80f6d3904bad223fd29b29c10b61b67a843e
Jasmeee <jasmineaubrey123@gmail.com>
2023-03-25 03:48:50 +0700

    add catalog page

---
+++ src/Pages/Catalog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar2 from '../Components/Navbar2'
+import NewProduk from '../Components/NewProduk'
+
+const Catalog = () => {
+  return (
+    <div>
+      <Navbar2/>
+      <NewProduk/>
+    </div>
+  )
+}
+
+export default Catalog
